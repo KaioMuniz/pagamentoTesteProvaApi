9cb8888b20ed1e86f2785ddb576e4b9209221892
KaioMuniz <kkaioribeiro@gmail.com>
2025-07-01 14:52:33 -0300

    ajustes

---
+++ scripts.sql
@@ -0,0 +1,13 @@
+INSERT INTO produto (id, nome, preco) VALUES
+(gen_random_uuid(), 'Produto 1', 19.99),
+(gen_random_uuid(), 'Produto 2', 29.99),
+(gen_random_uuid(), 'Produto 3', 39.99),
+(gen_random_uuid(), 'Produto 4', 49.99),
+(gen_random_uuid(), 'Produto 5', 59.99),
+(gen_random_uuid(), 'Produto 6', 69.99),
+(gen_random_uuid(), 'Produto 7', 79.99),
+(gen_random_uuid(), 'Produto 8', 89.99),
+(gen_random_uuid(), 'Produto 9', 99.99),
+(gen_random_uuid(), 'Produto 10', 109.99);
+
+SELECT * FROM produto;
--- src/main/java/br/com/kaiomuniz/domain/entities/Produto.java
+++ src/main/java/br/com/kaiomuniz/domain/entities/Produto.java
@@ -2,14 +2,26 @@ package br.com.kaiomuniz.domain.entities;
 
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "produto")
 public class Produto {
 
-	private UUID id;
-	private String nome;
-	private Double preco;
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String nome;
+
+    private Double preco;
+
+    
 
 }
